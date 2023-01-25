b38824e651f01fa1248d360901464f8cc1317d30
Jonathan Lim <jonathan.lim@u.nus.edu>
2023-01-24 23:05:06 -0500

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Embedding-based search for Arxiv papers',
+    author='jonathanlimsc',
+    license='MIT',
+)
