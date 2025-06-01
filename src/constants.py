9cf337e3006df2dc4582a24e6d335794a14c3267
Jonathan Lim <jonathan.lim@u.nus.edu>
2025-05-31 22:06:16 -0400

    Set MAX_CHARS=300 and num papers to retrieve to be 500

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 # Arxiv categories: refer to https://arxiv.org/category_taxonomy
 ARXIV_CATEGORIES = ["cat:cs.AI", "cat:cs.CV", "cat:cs.LG", "cat:cs.IT", "cat:cs.ET", "cat:cs.MA", "cat:cs.NE", "cat:cs.RO", "cat:cs.SD"]
-MAX_CHARS = 750
-DEFAULT_MAX_NUM_PAPERS_TO_RETRIEVE = 1000
+MAX_CHARS = 300
+DEFAULT_MAX_NUM_PAPERS_TO_RETRIEVE = 500
 DEFAULT_TOP_K = 30
